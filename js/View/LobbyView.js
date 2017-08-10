e855521bf6fde74d13487d72523ca1494fc7a84d
Marijn Heuts <m.heuts@live.nl>
2017-08-10 09:09:49 +0200

    created button elements

--- js/View/LobbyView.js
+++ js/View/LobbyView.js
@@ -1,7 +1,11 @@
 class lobbyView{
 
     constructor(container){
-    this.container = document.getElementById(container);
+        this.container = document.getElementById(container);
+        this.list = document.createElement('li');
+        this.continue = document.createElement('button');
+        this.vsAi = document.createElement('button');
+        this.vsPlayer = document.createElement('button');
     }
 
     show(){
