25103badd15a0c489cad8262a9a38a183dbbb15e
Marijn Heuts <m.heuts@live.nl>
2017-08-15 14:05:36 +0200

    added build board

--- js/View/GameView.js
+++ js/View/GameView.js
@@ -1,9 +1,22 @@
 class GameView {
     constructor(container){
         this.container = document.getElementById(container);
+        this.Board = document.createElement('table');
     }
 
     show(){
         this.container.innerHTML = "";
+    }
+    
+    buildBoard(){
+        for(x = 0; x<=10 ; x++){
+            let row = document.createElement('tr');
+            this.Board.appendChild(row);
+            for(y = 0; y<=10; y++){
+                let field = document.createElement('td');
+                square.id = x + ", " + y;
+                row.appendChild(field);
+            }
+        }
     }
 }
