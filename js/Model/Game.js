5d6696e25fd4739ce34eabea2b32e83aa68eb55e
Marijn Heuts <m.heuts@live.nl>
2017-08-16 09:02:39 +0200

    Update Game.js

--- js/Model/Game.js
+++ js/Model/Game.js
@@ -3,6 +3,9 @@ class gameModel{
     set openGame(Game){
         console.log(Game);
         this.id = Game.id;
+        this.opponent = Game.opponent;
+        this.winner = Game.Winner;
+        this.state = Game.state;
     }
 
 }
